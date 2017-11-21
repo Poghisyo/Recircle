bfb8f0f605bf5d38e39b5716633b2774b4bf4d50
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-21 17:35:54 +0100

    added login/signup with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Following line makes your model google-omniauthable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
   has_many :materials
   has_many :purchases
+
+
+  def self.find_for_google_oauth2(oauth, signed_in_resource=nil)
+    credentials = oauth.credentials
+    data = oauth.info
+    user = User.where(email: data["email"]).first
+
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+    user = User.create(
+      first_name: data["first_name"],
+      last_name: data["first_name"],
+      picture: data["image"],
+      email: data["email"],
+      password: Devise.friendly_token[0,20],
+      token: credentials.token,
+      refresh_token: credentials.refresh_token
+     )
+    end
+
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks"  }
   root to: 'pages#home'
 
   resource :profile, except: [:new, :create, :destroy]# only: [:new, :create, :edit, :update]
