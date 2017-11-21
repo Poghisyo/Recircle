b08b3464b16b6178bcb414abce2602f731e55891
victor laurent <victor.laurent@gmail.com>
2017-11-22 00:32:41 +0100

    preview finished card, updated components, JS animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,12 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
+$(document).ready(function($) {
+    $('.card__share > a').on('click', function(e){
+        e.preventDefault() // prevent default action - hash doesn't appear in url
+        $(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
+        $(this).toggleClass('share-expanded');
+    });
+});
+
