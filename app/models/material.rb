baa592daec7d9c9acc63b9470617c693b107e5d8
victor laurent <victor.laurent@gmail.com>
2017-11-21 16:39:14 +0100

    created the index page and the cards and the geolocalisation

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,9 @@
 class Material < ApplicationRecord
+  after_validation :geocode, if: :address_changed?
   belongs_to :seller, class_name: 'User', foreign_key: 'user_id'
 
   has_many :purchase
   validates :title, presence: true
+
+  geocoded_by :address
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'materials/index'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # As buyer
+  resources :materials, only: [:index]
+
+  #As Seller
+  resources :materials, only: [:index]
+
 end
