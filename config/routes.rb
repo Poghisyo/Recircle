6f5ebb2723e5810036591ea21a15273c002e8209
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-22 15:07:27 +0100

    add dashboard show and tab menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+
+
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks"  }
 
   root to: 'pages#home'
 
   resource :profile, except: [:new, :create, :destroy]# only: [:new, :create, :edit, :update]
+  resource :dashboard, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #As Seller
