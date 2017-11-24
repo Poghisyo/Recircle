19f13359fcfb449a1a4e26d3d2b4733ee81a7b95
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-24 11:32:37 +0100

    booking button done... NO STYLING! still to

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,11 +10,20 @@ class PurchasesController < ApplicationController
   def create
     purchase = Purchase.new
     purchase.buyer = current_user
-    purchase.material_id = params[:material_id]
+    mater = Material.find(params[:material_id])
+    purchase.material = mater
     purchase.status = 'pending'
+    byebug
     purchase.save!
 
     redirect_to dashboard_path(active: 'purchases')
+  end
+
+
+  private
+
+  def purchase_params
+    params.require(:material_id).permit(:material_id)
   end
 
 end
