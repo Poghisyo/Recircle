1558755201355265b9facd971e49e4475afe5933
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-25 00:08:12 +0100

    add my-materials index page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,6 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @purchases = Purchase.where(buyer: current_user)
-    @materials = Material.where(user: current_user)
+    @materials = Material.where(seller: current_user)
   end
 end
