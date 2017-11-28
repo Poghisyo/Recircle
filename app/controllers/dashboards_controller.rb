e21e135f92f695104f68e0e3456202ffec67e67b
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-28 13:49:22 +0100

    inbox display setup... needs styling... TODO: if have time need to redo so group by chat...

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,14 @@ class DashboardsController < ApplicationController
     @user = current_user
     @purchases = Purchase.where(buyer: current_user)
     @materials = Material.where(seller: current_user)
+
     @chats = Chat.where(receiver: current_user)
     @sent_chats = Chat.where(sender: current_user)
+    @all_chats = @chats + @sent_chats
+
+    @messages = []
+    @chats.order(updated_at: :desc).each do |chat|
+      @messages += Message.where(chat_id: chat).order(updated_at: :desc)
+    end
   end
 end
