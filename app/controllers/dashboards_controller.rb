ac28c4557c10437020fabb06c7d32824253615e6
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-29 13:25:55 +0100

    add favourite button color and instance variable in dash controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -12,5 +12,7 @@ class DashboardsController < ApplicationController
     @chats.order(updated_at: :desc).each do |chat|
       @messages += Message.where(chat_id: chat).order(updated_at: :desc)
     end
+
+    @favourites = Favourite.pluck(user_id: current_user)
   end
 end
