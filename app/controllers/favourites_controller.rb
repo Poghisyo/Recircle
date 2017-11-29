ede2eb14dd2db2f933d24c6e426db3e6b3fb3616
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-29 15:04:41 +0100

    remove Logo file REMOVE BYEBUG from favourites

---
+++ app/controllers/favourites_controller.rb
@@ -0,0 +1,17 @@
+class FavouritesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @fav = Favourite.new
+    @fav.material_id = params[:material_id]
+    @fav.user_id = current_user.id
+    @fav.save!
+    @material = Material.find(params[:material_id])
+    redirect_to(:back)
+    flash[:notice] = "#{@material.title} has been added to your wishlist"
+  end
+end
